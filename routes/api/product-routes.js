8d033797c86430d1d56cff92b9583e986ae4b764
Mital <mitalg0717@gmail.com>
2021-11-21 18:44:32 -0500

    edidted the code in produt route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   Product.findOne({
     where: {
-      id: req.params.id,
+      id: req.params.id
     },
     attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
     include: [
@@ -73,16 +73,9 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    category_id: req.body.category_id,
-    tag_id: req.body.tag_id
-
-  })
+  Product.create(req.body)
+  
     
-
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
